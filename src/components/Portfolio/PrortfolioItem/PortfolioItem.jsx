75d6668959dbdcd753e2f734e5e4efece307ebee
AlexandrReactJs <108229249+AlexandrReactJs@users.noreply.github.com>
2022-07-05 23:10:50 +0300

    layout

--- src/components/Portfolio/PrortfolioItem/PortfolioItem.jsx
+++ src/components/Portfolio/PrortfolioItem/PortfolioItem.jsx
@@ -8,9 +8,13 @@ const PortfolioItem = () => {
                 <img src="https://miro.medium.com/max/1200/1*BWsJTLW-UTO2hmvA9b92pw.jpeg" alt="" />
                 <div className={style.info}>
                     <h3>React social network</h3>
+                    <div className={style.underline}></div>
                     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officiis 
                         recusandae vitae natus error labore voluptatem velit maxime quae? Hic
                          veritatis numquam illum et saepe fugiat error rem libero voluptates?</p>
+                </div>
+                <div className={style.link}>
+                    <a href="#">Открыть на GitHub</a>
                 </div>
                 
             </div>
